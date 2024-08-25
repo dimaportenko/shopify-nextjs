353665b2f43890d41e4be3315954a58a4c29df0a
Dima Portenko <troublesohard@gmail.com>
2024-08-25 20:39:30 +0300

    update deprecated components for headlessui

--- src/components/layout/navbar/mobile-menu.tsx
+++ src/components/layout/navbar/mobile-menu.tsx
@@ -1,13 +1,12 @@
-'use client';
+"use client";
 
-import { Dialog, Transition } from '@headlessui/react';
-import Link from 'next/link';
-import { usePathname, useSearchParams } from 'next/navigation';
-import { Fragment, Suspense, useEffect, useState } from 'react';
+import { Dialog, DialogPanel, Transition, TransitionChild } from "@headlessui/react";
+import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
+import { Fragment, useEffect, useState } from "react";
 
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
-import { Menu } from '@/types/storefront.types';
-import { MenuDTO } from '@/services/shopify/config/config.dto';
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { MenuDTO } from "@/services/shopify/config/config.dto";
 // import Search, { SearchSkeleton } from './search';
 
 export default function MobileMenu({ menu }: { menu: MenuDTO }) {
@@ -23,8 +22,8 @@ export default function MobileMenu({ menu }: { menu: MenuDTO }) {
         setIsOpen(false);
       }
     };
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, [isOpen]);
 
   useEffect(() => {
@@ -42,7 +41,7 @@ export default function MobileMenu({ menu }: { menu: MenuDTO }) {
       </button>
       <Transition show={isOpen}>
         <Dialog onClose={closeMobileMenu} className="relative z-50">
-          <Transition.Child
+          <TransitionChild
             as={Fragment}
             enter="transition-all ease-in-out duration-300"
             enterFrom="opacity-0 backdrop-blur-none"
@@ -52,8 +51,8 @@ export default function MobileMenu({ menu }: { menu: MenuDTO }) {
             leaveTo="opacity-0 backdrop-blur-none"
           >
             <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
-          </Transition.Child>
-          <Transition.Child
+          </TransitionChild>
+          <TransitionChild
             as={Fragment}
             enter="transition-all ease-in-out duration-300"
             enterFrom="translate-x-[-100%]"
@@ -62,10 +61,10 @@ export default function MobileMenu({ menu }: { menu: MenuDTO }) {
             leaveFrom="translate-x-0"
             leaveTo="translate-x-[-100%]"
           >
-            <Dialog.Panel className="fixed bottom-0 left-0 right-0 top-0 flex h-full w-full flex-col bg-white pb-6 dark:bg-black">
+            <DialogPanel className="fixed bottom-0 left-0 right-0 top-0 flex h-full w-full flex-col bg-white pb-6">
               <div className="p-4 flex flex-col">
                 <button
-                  className="self-end mb-4 flex h-11 w-11 items-center justify-center rounded-md text-black transition-colors dark:border-neutral-700 dark:text-white"
+                  className="self-end mb-4 flex h-11 w-11 items-center justify-center rounded-md text-black transition-colors"
                   onClick={closeMobileMenu}
                   aria-label="Close mobile menu"
                 >
@@ -81,7 +80,7 @@ export default function MobileMenu({ menu }: { menu: MenuDTO }) {
                   <ul className="flex w-full flex-col">
                     {menu.items.map((item) => (
                       <li
-                        className="py-2 text-xl text-black transition-colors hover:text-neutral-500 dark:text-white"
+                        className="py-2 text-xl text-black transition-colors hover:text-neutral-500"
                         key={item.title}
                       >
                         <Link href={item.path} prefetch={true} onClick={closeMobileMenu}>
@@ -92,8 +91,8 @@ export default function MobileMenu({ menu }: { menu: MenuDTO }) {
                   </ul>
                 ) : null}
               </div>
-            </Dialog.Panel>
-          </Transition.Child>
+            </DialogPanel>
+          </TransitionChild>
         </Dialog>
       </Transition>
     </>
